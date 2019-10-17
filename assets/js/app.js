3ab703209c626b5ec0f924fff7d70b1a529c0f35
William Sams III <w.sams11@yahoo.com>
2019-10-17 15:44:25 -0700

    javascript final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var specials = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
+var specials = '!@#$%^&*()_+{}:"<>?|[];\',./`~';
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '0123456789';
@@ -6,11 +6,12 @@ var passwordChars = [];
 
 
 document.getElementById("password-btn").addEventListener("click", function () {
-    getPasswordOptions();
-})
+    passwordGenerate();
+});
+
 function getPasswordOptions(passwordOptions) {
     var length = parseInt
-    (prompt("how many characters would you like your password to contain?"))
+        (prompt("how many characters would you like your password to contain?"))
     if (isNaN(length)) {
         alert("password length must be a number");
         return;
@@ -24,33 +25,77 @@ function getPasswordOptions(passwordOptions) {
         return;
     }
     var specialChars = confirm("would you like to use special characters?");
-    
+
     var numericChars = confirm("would you like to use numeric characters?");
-    
+
     var uppercaseChars = confirm("would you like to use Uppercase characters?");
-    
+
     var lowercaseChars = confirm("would you like to use lowercase characters?");
-    
+
     if (!specialChars && !numericChars && !uppercaseChars && !lowercaseChars) {
         alert("We can't help you, you are too picky");
         return;
     }
-   var passwordOptions = {
+    var passwordOptions = {
+        length: length,
         specialChars: specialChars,
         numericChars: numericChars,
         uppercaseChars: uppercaseChars,
         lowercaseChars: lowercaseChars,
     }
-    
-    
-    console.log(passwordOptions);
+
     return passwordOptions;
-    
-    
+
+}
+
+function getRandom(str) {
+    return str.charAt(Math.floor(Math.random() * str.length));
 }
 
 function passwordGenerate() {
-    console.log(passwordOptions);
+    var options = getPasswordOptions();
+    if (!options) return;
+
+    var password = "";
+
+    if (options.specialChars) {
+        password += getRandom(specials);
+    }
+
+    if (options.numericChars) {
+        password += getRandom(numbers);
+    }
+
+    if (options.uppercaseChars) {
+        password += getRandom(uppercase);
+    }
+
+    if (options.lowercaseChars) {
+        password += getRandom(lowercase);
+    }
+
+    for (var i = 0; i < options.length; i++) {
+        if (options.specialChars) {
+            password += getRandom(specials);
+        }
+
+        if (options.numericChars) {
+            password += getRandom(numbers);
+        }
+
+        if (options.uppercaseChars) {
+            password += getRandom(uppercase);
+        }
+
+        if (options.lowercaseChars) {
+            password += getRandom(lowercase);
+        }
+    }
+    
+    
+    document.getElementById("password").innerText = password.substring(0, options.length)
+
+
 }
-passwordGenerate()
+
 // getPasswordOptions();
